1e0b9383f1be81e411f354546cffb94c48e529ce
YuriLealS <yuri.sapuppo@sptech.school>
2024-10-17 18:03:22 -0300

    Banner e animações

---
+++ site/teste/script.js
@@ -0,0 +1,8 @@
+// script.js
+window.addEventListener("load", function() {
+    // Seleciona o elemento com a classe 'fade-in-box'
+    var fadeInBox = document.querySelector(".fade-in-box");
+    
+    // Adiciona a classe 'show' após o carregamento da página
+    fadeInBox.classList.add("show");
+});
