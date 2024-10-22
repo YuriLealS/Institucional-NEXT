d4cd08ba42d751ec1d2f44665da0adfa6d2e0a46
YuriLealS <yuri.sapuppo@sptech.school>
2024-10-22 17:50:18 -0300

    Pagina do sobre com post do instagram

--- site/public/js/script.js
+++ site/public/js/script.js
@@ -2,10 +2,10 @@
 function isElementInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
@@ -13,9 +13,9 @@ function isElementInViewport(el) {
 function checkFadeInElements() {
   const elements = document.querySelectorAll('.fadeIn'); // Seleciona todos os elementos com a classe 'fadeIn'
   elements.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('fadeIn'); // Adiciona a classe que ativa a animação
-    }
+      if (isElementInViewport(element)) {
+          element.classList.add('fadeIn'); // Adiciona a classe que ativa a animação
+      }
   });
 }
 
@@ -25,18 +25,18 @@ function handleHeaderVisibility() {
   const header = document.querySelector('.cabecalho'); // O elemento do cabeçalho
 
   window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset; // Posição atual do scroll
+      let currentScrollPos = window.pageYOffset; // Posição atual do scroll
 
-    if (prevScrollPos > currentScrollPos) {
-      // Rolou para cima
-      header.classList.remove('hidden');
-    } else {
-      // Rolou para baixo
-      header.classList.add('hidden');
-    }
+      if (prevScrollPos > currentScrollPos) {
+          // Rolou para cima
+          header.classList.remove('hidden');
+      } else {
+          // Rolou para baixo
+          header.classList.add('hidden');
+      }
 
-    // Atualiza a posição anterior do scroll
-    prevScrollPos = currentScrollPos;
+      // Atualiza a posição anterior do scroll
+      prevScrollPos = currentScrollPos;
   };
 }
 
@@ -50,10 +50,10 @@ window.addEventListener('scroll', () => {
   checkFadeInElements(); // Verifica fade-in quando rola a página
 });
 
- // Função para alterar a logo conforme o tamanho da tela
- function verificarTamanhoTela() {
+// Função para alterar a logo conforme o tamanho da tela
+function verificarTamanhoTela() {
   const logo = document.getElementById('logo');
-  if (window.innerWidth <= 500) {  // Considera telas menores que 768px como dispositivos móveis
+  if (window.innerWidth <= 500) {  // Considera telas menores que 500px como dispositivos móveis
       logo.src = 'css/img/logo.png'; // Altere o caminho para sua logo pequena
   } else {
       logo.src = 'css/img/logo-escura.png'; // Altere o caminho para sua logo padrão
@@ -65,3 +65,8 @@ verificarTamanhoTela();
 
 // Adicionar o evento de resize para que a logo mude quando o usuário redimensionar a janela
 window.addEventListener('resize', verificarTamanhoTela);
+
+// Função para esconder o bloco do Instagram
+document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('instagramEmbed').style.display = 'none';
+});
